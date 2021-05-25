6a964921d0941a57b79fd12c977f4fade4fdab5d
Travis Ledo <travis.ledo@gmail.com>
2021-05-25 02:12:58 -0500

    Add Footer Padding

--- react-portfolio/src/components/footer.js
+++ react-portfolio/src/components/footer.js
@@ -5,7 +5,7 @@ import "./footer.css";
 const Footer = () => {
   return (
     <div id="footer">
-      <div className="d-flex align-items-center justify-content-center">
+      <div className="d-flex align-items-center justify-content-center pt-5">
         <span>© 2021 Travis Ledo</span>
       </div>
     </div>
